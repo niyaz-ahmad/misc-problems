c115dedaa20ad175397ffeaf9f14209dcc549c4a
niyaz-ahmad <69178421+niyaz-ahmad@users.noreply.github.com>
2023-02-04 16:14:11 +0530

    final code

--- python/organic_binary_tree.py
+++ python/organic_binary_tree.py
@@ -16,11 +16,12 @@ class Cell:
     def cleanFloat(val:int|float):
         if isinstance(val, float):
             rounded = round(val)
-            if rounded == val:
+            if abs(rounded-val)<0.011:
                 return rounded
+            else:
+                return round(val, 2)
         return val
-        
-            
+
 
 class Leaf(Cell):
     def __init__(self, val:int|float):
@@ -31,7 +32,7 @@ class Leaf(Cell):
         return self.cleanFloat(self.__val)
     @val.setter
     def val(self, val:int|float):
-        self.__val = val
+        self.__val = self.cleanFloat(val)
     
     def __call__(self, val:int|float=None):
         if val == None:
@@ -45,54 +46,24 @@ class Leaf(Cell):
         return f"{self.val}"
         
 class Node(Cell):
-    def __init__(self, ltNode:Cell, rtNode:Cell, op:str, preferredNode:Cell=None):
+    def __init__(self, ltNode:Cell, rtNode:Cell, op:str, 
+                 prefPath:str='right'):
         self.ltNode = ltNode
         self.rtNode = rtNode
         self.op = op
-        self.preferredNode = preferredNode
-
-    @property
-    def op(self):
-        return self.__op
-    @op.setter
-    def op(self, op:str):
-        self.__op = op
-                
-    @property
-    def ltNode(self):
-        return self.__ltNode
-    @ltNode.setter
-    def ltNode(self, ltNode:Cell):
-        self.__ltNode = ltNode
-
-    @property
-    def rtNode(self):
-        return self.__rtNode
-    @rtNode.setter
-    def rtNode(self, rtNode:Cell):
-        self.__rtNode = rtNode
+        self.prefPath = prefPath
 
     @property
     def preferredNode(self):
-        return self.__preferredNode
-    @preferredNode.setter
-    def preferredNode(self, which:Cell):
-        if which == None:
-            self.__preferredNode = self.rtNode
-        else:
-            self.__preferredNode = which
+        match self.prefPath:
+            case 'left' : return self.ltNode
+            case 'right': return self.rtNode
 
     @property
     def unPreferredNode(self):
-        if self.preferredNode == self.ltNode:
-            return self.rtNode
-        else:
-            return self.ltNode
-        
-    def __repr__(self):
-        return f"Node( {repr(self.ltNode)}, {repr(self.rtNode)}, {repr(self.op)} )" #, {repr(self.preferredNode)} )"
-    def __str__(self):
-        return f"( {str(self.ltNode)} {self.op} {str(self.rtNode)} )"
+        match self.prefPath:
+            case 'left' : return self.rtNode
+            case 'right': return self.ltNode
 
     def __call__(self, val :int|float = None):
         if val == None:
@@ -107,47 +78,71 @@ class Node(Cell):
                     nodeVal =  self.ltNode() / self.rtNode()
                 case _:
                     raise
-            
             return self.cleanFloat(nodeVal)
         else:
             match self.op:
                 case '+':
                     self.preferredNode( val - self.unPreferredNode() )
-                case '-':
-                    self.preferredNode( val + self.unPreferredNode() )
                 case '*':
                     self.preferredNode( val / self.unPreferredNode() )
+                case '-':
+                    match self.prefPath:
+                        case 'left'  : 
+                            self.preferredNode( val + self.unPreferredNode() )
+                        case 'right' : 
+                            self.preferredNode( self.unPreferredNode() - val )
                 case '/':
-                    self.preferredNode( val * self.unPreferredNode() )
+                    match self.prefPath:
+                        case 'left ' : 
+                            self.preferredNode( val * self.unPreferredNode() )
+                        case 'right' : 
+                            self.preferredNode( self.unPreferredNode() / val )
                 case _:
                     raise
+        
+    def __repr__(self):
+        return f"Node( {repr(self.ltNode)}, {repr(self.rtNode)}, {repr(self.op)} )" #, {repr(self.preferredNode)} )"
+    def __str__(self):
+        return f"( {str(self.ltNode)} {self.op} {str(self.rtNode)} )"
                 
 
 if __name__ == '__main__':
+    # create new Leaf nodes having values
     a = Leaf(2)
     b = Leaf(3)
     c = Leaf(4)
-    d = a+b*c       # (b*c) is not
-    e = d - Leaf(3)
-
-    print(f"\n{e} = {e()}\n")
     
-    print('## The dictionary of values has only optical purpose,')
-    print(f'Printing dictionary like structure should be handled externally like this:')
-    print( f"{e} => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
+    # define relationships
+    d = a + b*c       # (b*c) becomes anonymous
+    e = d - Leaf(3)   # Leaf(3) is anonymous
 
+    print('\nget values of known nodes (also note the anonymous nodes):')
+    print(f"{e} => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
+    
+
+    print('update values:')
     a(1)
     print(f"a(1) => {e} = {e()}")
     print( f"a(1) => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
+    
     b(2)
     print(f"b(2) => {e} = {e()}")
     print( f"b(2) => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
+    
     c(3)
     print(f"c(3) => {e} = {e()}")
     print( f"c(3) => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
 
     d(627)
     print(f"d(627) => {e} = {e()}")
-    print( f"d(627) => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
+    print(f"d(627) => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
     
-    
+    e(10)
+    print(f"e(10) => {e} = {e()}")
+    print(f"e(10) => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
+
+    # change ordering preference
+    e.prefPath = 'left'
+    e(100)
+    print(f"e(100) => {e} = {e()}")
+    print(f"e(100) => {{ 'a':{a()}, 'b':{b()}, 'c':{c()}, 'd':{d()}, 'e':{e()} }}\n")
